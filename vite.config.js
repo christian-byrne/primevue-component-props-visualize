198bfb4dc7833f942e3cf85da58c9c774c1a2ac7
bymyself <cbyrne@comfy.org>
2025-06-13 15:08:29 -0700

    [fix] Update base path for correct GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/primevue-component-playground/',
+  base: '/primevue-component-props-visualize/',
   server: {
     port: 3000,
     open: true
